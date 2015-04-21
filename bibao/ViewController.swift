128302d0879239e9e44ea66b6d82605a3603d7e2
duck <duck@duck.local>
2015-04-21 09:23:12 +0800

    Initial Commit

---
+++ bibao/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bibao
+//
+//  Created by duck on 15/4/21.
+//  Copyright (c) 2015年 Duck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
